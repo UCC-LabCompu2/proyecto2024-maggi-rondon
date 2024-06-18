95e4fddc722f603b56f27d8c507bf5abcb1da763
JRondon23 <rondonjuanignaio15@gmail.com>
2024-06-18 17:52:38 -0300

    falta corregir

--- misfunciones.js
+++ misfunciones.js
@@ -1,69 +1,98 @@
-
-
-
-
-let mostrarcancion=(valor)=>{
-    if (valor==="Fanky"){
-        document.getElementById("imgSupersonic").style.display='none'
-        document.getElementById("imgCyclone").style.display='none'
-        document.getElementById("imgFanky").style.display='block'
+/*
+* funcion que muestra la imagen de la cancion seleccionada por el usuario en el select "canción"
+* @method Mostrarcancion
+* @param {string} valor - Almacena el valor seleccionado por el usuario
+* @return Ejecuta en el css, display: none o display: block, segun corresponda
+* */
+let Mostrarcancion = valor => {
+    if (valor === "Fanky") {
+        document.getElementById("imgSupersonic").style.display = 'none'
+        document.getElementById("imgCyclone").style.display = 'none'
+        document.getElementById("imgFanky").style.display = 'block'
     }
-     if (valor==="Supersonic"){
-        document.getElementById("imgFanky").style.display='none'
-        document.getElementById("imgCyclone").style.display='none'
-         document.getElementById("imgSupersonic").style.display='block'
+    if (valor === "Supersonic") {
+        document.getElementById("imgFanky").style.display = 'none'
+        document.getElementById("imgCyclone").style.display = 'none'
+        document.getElementById("imgSupersonic").style.display = 'block'
     }
-    if (valor==="Cyclone"){
-        document.getElementById("imgFanky").style.display='none'
-        document.getElementById("imgSupersonic").style.display='none'
-        document.getElementById("imgCyclone").style.display='block'
+    if (valor === "Cyclone") {
+        document.getElementById("imgFanky").style.display = 'none'
+        document.getElementById("imgSupersonic").style.display = 'none'
+        document.getElementById("imgCyclone").style.display = 'block'
     }
-
 
 }
+/*
+* Funcion que muestra las imagenes de las canciones que corresponden con el género musical seleccionado por el usuario en el selector "genero"
+* @method Mostrargenero
+* @param {string} valor - Almacena el valor seleccionado por el usuario
+* @return Ejecuta en el css, display: none o display: block, segun corresponda
+* */
 
-let mostrargenero=(valor)=>{
-    if (valor==="rock"){
-        document.getElementById("imgFanky").style.display='block'
-        document.getElementById("imgSupersonic").style.display='block'
-        document.getElementById("imgCyclone").style.display='none'
+
+let Mostrargenero = valor => {
+    if (valor === "rock") {
+        document.getElementById("imgFanky").style.display = 'block'
+        document.getElementById("imgSupersonic").style.display = 'block'
+        document.getElementById("imgCyclone").style.display = 'none'
     }
-    if (valor==="pop"){
-        document.getElementById("imgFanky").style.display='none'
-        document.getElementById("imgSupersonic").style.display='none'
-        document.getElementById("imgCyclone").style.display='block'
+    if (valor === "pop") {
+        document.getElementById("imgFanky").style.display = 'none'
+        document.getElementById("imgSupersonic").style.display = 'none'
+        document.getElementById("imgCyclone").style.display = 'block'
     }
-    if (valor==="reggae"){
+    if (valor === "reggae") {
         alert('NINGUNA CANCIÓN DE ESTE GENERO')
-        document.getElementById("genero").value=" "
+        document.getElementById("genero").value = "reggae"
+        document.getElementById("imgFanky").style.display = 'none'
+        document.getElementById("imgSupersonic").style.display = 'none'
+        document.getElementById("imgCyclone").style.display = 'none'
     }
+}
+
+/*
+* Funcion que muestra las imagenes de las canciones que corresponden con el artista buscado por el usuario en el intpu "artista"
+* @method Mostrarartista
+* @param {string} valor - Almacena el valor ingresado por el usuario
+* @return Ejecuta en el css, display: none o display: block, segun corresponda
+* */
+
+
+let Mostrarartista = valor => {
+    valor = valor.toLowerCase() //Funcion que transforma la cadena de texto en minuscula
+    if (valor === "charly" || valor === "charly garcia") {
+        document.getElementById("imgFanky").style.display = 'block'
+        document.getElementById("imgSupersonic").style.display = 'none'
+        document.getElementById("imgCyclone").style.display = 'none'
+    } else if (valor === "oasis") {
+        document.getElementById("imgFanky").style.display = 'none'
+        document.getElementById("imgSupersonic").style.display = 'block'
+        document.getElementById("imgCyclone").style.display = 'none'
+    } else if (valor === "sticky" || valor === "sticky fingers") {
+        document.getElementById("imgFanky").style.display = 'none'
+        document.getElementById("imgSupersonic").style.display = 'none'
+        document.getElementById("imgCyclone").style.display = 'block'
+    } else {
+        alert('No existen valores coincidentes con esa busqueda')
+        document.getElementById("imgFanky").style.display = 'block'
+        document.getElementById("imgSupersonic").style.display = 'block'
+        document.getElementById("imgCyclone").style.display = 'block'
+    }
+
 
 }
-let mostrarartista=(valor)=>{
-    if (valor==="charly"||valor==="Charly"||valor==="charly garcia"||valor==="Charly Garcia"||valor==="charly Garcia"){
-        document.getElementById("imgFanky").style.display='block'
-        document.getElementById("imgSupersonic").style.display='none'
-        document.getElementById("imgCyclone").style.display='none'
-    }
-    else if (valor==="oasis"||valor==="Oasis"){
-        document.getElementById("imgFanky").style.display='none'
-        document.getElementById("imgSupersonic").style.display='block'
-        document.getElementById("imgCyclone").style.display='none'
-    }
-    else if (valor==="sticky"||valor==="Sticky"||valor==="sticky fingers"||valor==="Sticky fingers"||valor==="Sticky Fingers"){
-        document.getElementById("imgFanky").style.display='none'
-        document.getElementById("imgSupersonic").style.display='none'
-        document.getElementById("imgCyclone").style.display='block'
-    }
-    else if(valor===" "){
-        document.getElementById("imgFanky").style.display='block'
-        document.getElementById("imgSupersonic").style.display='block'
-        document.getElementById("imgCyclone").style.display='block'
-    }
-    else{
-        alert('El artista no se encuentra en nuestras bibliotecas o intente de otra forma')
-    }
+/*
+* Esta funcion genera el canva
+* @method Mostrarcanva
+* */
 
 
-
+let Mostrarcanva = () => {
+    const canva = document.getElementById("canvas");
+    const ctx = canva.getContext("2d");
+    const img = new Image();
+    img.src = "imagenes/spinetta.jpg";
+    const newWidth = canva.width; //le aplico a la imagen el mismo tamaño que el canva
+    const newHeight = canva.height;
+    ctx.drawImage(img, 0, 0, newWidth, newHeight);
 }
